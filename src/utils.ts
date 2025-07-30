b43aa0e64a562607cb0a1d99627c75e0a8f1c92b
최준영 <jychoi@jobis.co>
2025-07-30 18:36:08 +0900

    typescript migration

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+import * as path from 'path';
+import * as os from 'os';
+
+export const getBunEnvironment = (): NodeJS.ProcessEnv => {
+  const homeDir = os.homedir();
+  const bunPath = path.join(homeDir, '.bun', 'bin');
+  
+  return {
+    ...process.env,
+    PATH: `${bunPath}${path.delimiter}${process.env.PATH || '/usr/bin:/bin'}`
+  };
+};
+
+export const getCcusageCommand = (command: string): string => {
+  return `bunx ccusage ${command}`;
+};
